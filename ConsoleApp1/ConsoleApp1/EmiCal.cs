5edd94df4c5d88e94c89238e60ef4f7cdae827d8
Amol-salelkar-kilowott <amol.salelkar@kilowott.com>
2021-12-06 17:36:14 -0800

    EmiCalulation

---
+++ ConsoleApp1/ConsoleApp1/EmiCal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EmiCal
+    {
+        public double Emi_calculator(double p, double i, double t, double n)
+        {
+            i /= 100;
+            double emi = (p * Math.Pow((1 + i / n), (t * n))) / 24;
+            return (emi);
+        }
+
+    }
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class ConsoleApp1
+    {
+        
+        public static void Main(String[] args)
+        {
+            var take = new ConsoleApp1();
+            try
+            {
+                //taking user input
+                Console.WriteLine("EMI Calculator for 2 wheeler");
+                Console.WriteLine("Enter Principle Amount:");
+                double p = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Rate of Interest:");
+                double i = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter number of years:");
+                double t = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Monthly Compounding:");
+                double n = Convert.ToDouble(Console.ReadLine());
+
+                var SpObject = new EmiCal();
+                double emi = SpObject.Emi_calculator(p, i, t, n);
+                //print output
+                Console.WriteLine("Monthly EMI is:" + emi);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+        }
+
+    }
+}
